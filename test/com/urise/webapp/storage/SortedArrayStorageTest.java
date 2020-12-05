5ba655e4786a3414e88a4724e9d601a3f04f382b
doffleoni <doffleoni@users.noreply.github.com>
2020-12-05 10:24:39 +0300

    lesson4 est class should have exactly one public zero-argument constructor

---
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,9 @@
+package com.urise.webapp.storage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+
+    public SortedArrayStorageTest(Storage storage) {
+        super(new SortedArrayStorage());
+    }
+}
